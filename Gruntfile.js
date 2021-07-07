5d52b431d1f83dc0aef1f304eb06154b0a81dd07
vishwaksena-vishnu <85389699+vishwaksena-vishnu@users.noreply.github.com>
2021-07-07 22:04:12 +0530

    fix 0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,172 +1,168 @@
 "use strict";
 
 module.exports = function (grunt) {
-  require("time-grunt")(grunt);
 
-  require("jit-grunt")(grunt, {
-    useminPrepare: "grunt-usemin",
-  });
+    require('time-grunt')(grunt);
 
-  const sass = require("node-sass");
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
-  grunt.initConfig({
-    sass: {
-      dist: {
-        options: {
-          implementation: sass,
-          // sourceMap: true,
-          outputStyle: "expanded",
-        },
-        files: {
-          "css/styles.css": "css/styles.scss",
-        },
-      },
-    },
-    watch: {
-      files: "css/*.scss",
-      tasks: ["sass"],
-    },
-    browserSync: {
-      dev: {
-        bsFiles: {
-          src: ["css/*.css", "*.html", "js/*.js"],
-        },
-        options: {
-          watchTask: true,
-          server: {
-            baseDir: "./",
-          },
-        },
-      },
-    },
-    copy: {
-      html: {
-        files: [
-          {
-            expand: true,
-            dot: true,
-            cwd: "./",
-            src: ["*.html"],
-            dest: "dist",
-          },
-        ],
-      },
-      fonts: {
-        files: [
-          {
-            expand: true,
-            dot: true,
-            cwd: "node_modules/font-awesome",
-            src: ["fonts/*.*"],
-            dest: "dist",
-          },
-        ],
-      },
-    },
-    clean: {
-      build: {
-        src: ["dist/"],
-      },
-    },
-    imagemin: {
-      dynamic: {
-        files: [
-          {
-            expand: true,
-            dot: true,
-            cwd: "./",
-            src: ["img/*.{png,jpg,gif}"],
-            dest: "dist/",
-          },
-        ],
-      },
-    },
-    useminPrepare: {
-      foo: {
-        dest: "dist",
-        src: ["contactus.html", "aboutus.html", "index.html"],
-      },
-      options: {
-        flow: {
-          steps: {
-            css: ["cssmin"],
-            js: ["uglify"],
-          },
-          post: {
-            css: [
-              {
-                name: "cssmin",
-                createConfig: function (context, block) {
-                  var generated = context.options.generated;
-                  generated.options = {
-                    keepSpecialComments: 0,
-                    rebase: false,
-                  };
+    const sass = require("node-sass");
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    implementation: sass,
+                    
+                    outputStyle: "expanded",
                 },
-              },
-            ],
-          },
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
         },
-      },
-    },
-    concat: {
-      options: {
-        seperator: ";",
-      },
-      dist: {},
-    },
-    uglify: {
-      dist: {},
-    },
-    cssmin: {
-      dist: {},
-    },
-    filerev: {
-      options: {
-        encoding: "utf8",
-        algorithm: "md5",
-        length: 20,
-      },
-      release: {
-        files: [
-          {
-            src: ["dist/css/*.css", "dist/js/*.js"],
-          },
-        ],
-      },
-    },
-    usemin: {
-      html: ["dist/aboutus.html", "dist/contactus.html", "dist/index.html"],
-      options: {
-        assetsDirs: ["dist", "dist/css", "dist/js"],
-      },
-    },
-    htmlmin: {
-      dist: {
-        options: {
-          collapseWhitespace: true,
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
         },
-        files: {
-          "dist/index.html": "dist/index.html",
-          "dist/contactus.html": "dist/contactus.html",
-          "dist/aboutus.html": "dist/aboutus.html",
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
         },
-      },
-    },
-  });
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['img/*.{png,jpg,gif}'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['aboutus.html', 'contactus.html', 'index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function(context, block) {
+                                var generate = context.options.generated;
+                                generate.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                seperator: ';'
+            },
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: ['dist/aboutus.html', 'dist/contactus.html', 'dist/index.html'],
+            options: {
+                assetsDir: ['dir', 'dist/css', 'dist/js']
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/aboutus.html': 'dist/aboutus.html',
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/index.html': 'dist/index.html'
+                }
+            }
+        }
+    });
 
-  grunt.registerTask("css", ["sass"]);
-  grunt.registerTask("default", ["browserSync", "watch"]);
-  grunt.registerTask("build", [
-    "clean",
-    "copy",
-    "imagemin",
-    "useminPrepare",
-    "concat",
-    "cssmin",
-    "uglify",
-    "filerev",
-    "usemin",
-    "htmlmin",
-  ]);
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
 };
