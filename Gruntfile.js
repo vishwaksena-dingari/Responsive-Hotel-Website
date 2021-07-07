6d35c1bff8d1d2045a050268b1b4e288e902529b
vishwaksena-vishnu <85389699+vishwaksena-vishnu@users.noreply.github.com>
2021-07-07 16:13:19 +0530

    Grunt Part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,172 @@
-'use strict';
+"use strict";
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  require("time-grunt")(grunt);
 
-    require('time-grunt')(grunt);
-    
-    require("jit-grunt")(grunt);
+  require("jit-grunt")(grunt, {
+    useminPrepare: "grunt-usemin",
+  });
 
-    const sass = require("node-sass");
-    
-    grunt.initConfig({
-      sass: {
-        dist: {
-            options: {
-                implementation: sass,
-                // sourceMap: true,
-                outputStyle: 'expanded',
-            },
-            files: {
-                "css/styles.css": "css/styles.scss",
-            },
+  const sass = require("node-sass");
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+          implementation: sass,
+          // sourceMap: true,
+          outputStyle: "expanded",
+        },
+        files: {
+          "css/styles.css": "css/styles.scss",
         },
       },
-      watch: {
-        files: "css/*.scss",
-        tasks: ["sass"],
-      },
-      browserSync: {
-        dev: {
-          bsFiles: {
-            src: ["css/*.css", "*.html", "js/*.js"],
-          },
-          options: {
-            watchTask: true,
-            server: {
-              baseDir: "./",
-            },
+    },
+    watch: {
+      files: "css/*.scss",
+      tasks: ["sass"],
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: ["css/*.css", "*.html", "js/*.js"],
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./",
           },
         },
       },
-    });
+    },
+    copy: {
+      html: {
+        files: [
+          {
+            expand: true,
+            dot: true,
+            cwd: "./",
+            src: ["*.html"],
+            dest: "dist",
+          },
+        ],
+      },
+      fonts: {
+        files: [
+          {
+            expand: true,
+            dot: true,
+            cwd: "node_modules/font-awesome",
+            src: ["fonts/*.*"],
+            dest: "dist",
+          },
+        ],
+      },
+    },
+    clean: {
+      build: {
+        src: ["dist/"],
+      },
+    },
+    imagemin: {
+      dynamic: {
+        files: [
+          {
+            expand: true,
+            dot: true,
+            cwd: "./",
+            src: ["img/*.{png,jpg,gif}"],
+            dest: "dist/",
+          },
+        ],
+      },
+    },
+    useminPrepare: {
+      foo: {
+        dest: "dist",
+        src: ["contactus.html", "aboutus.html", "index.html"],
+      },
+      options: {
+        flow: {
+          steps: {
+            css: ["cssmin"],
+            js: ["uglify"],
+          },
+          post: {
+            css: [
+              {
+                name: "cssmin",
+                createConfig: function (context, block) {
+                  var generated = context.options.generated;
+                  generated.options = {
+                    keepSpecialComments: 0,
+                    rebase: false,
+                  };
+                },
+              },
+            ],
+          },
+        },
+      },
+    },
+    concat: {
+      options: {
+        seperator: ";",
+      },
+      dist: {},
+    },
+    uglify: {
+      dist: {},
+    },
+    cssmin: {
+      dist: {},
+    },
+    filerev: {
+      options: {
+        encoding: "utf8",
+        algorithm: "md5",
+        length: 20,
+      },
+      release: {
+        files: [
+          {
+            src: ["dist/css/*.css", "dist/js/*.js"],
+          },
+        ],
+      },
+    },
+    usemin: {
+      html: ["dist/aboutus.html", "dist/contactus.html", "dist/index.html"],
+      options: {
+        assetsDirs: ["dist", "dist/css", "dist/js"],
+      },
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+        },
+        files: {
+          "dist/index.html": "dist/index.html",
+          "dist/contactus.html": "dist/contactus.html",
+          "dist/aboutus.html": "dist/aboutus.html",
+        },
+      },
+    },
+  });
 
-    grunt.registerTask('css', ['sass']);
-    grunt.registerTask('default', ['browserSync', 'watch']);
+  grunt.registerTask("css", ["sass"]);
+  grunt.registerTask("default", ["browserSync", "watch"]);
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "imagemin",
+    "useminPrepare",
+    "concat",
+    "cssmin",
+    "uglify",
+    "filerev",
+    "usemin",
+    "htmlmin",
+  ]);
 };
